85d2f805840a3e9960e667c47729d769b89514c4
Spencer <131034335+CodingSpencer@users.noreply.github.com>
2026-06-25 07:54:51 -0600

    added header + footer partials, added home page, and added route to the home page to server.js

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
 
 const app = express();
 
+app.use(express.static('public'));
+
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+// Use express-ejs-layouts middleware
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+
+// Routes
+
+// Home page
+app.get('/', (req, res) => {
+    res.render("pages/index", { 
+        title: "Home | Cougar Car Rental",
+        user: null
+    });
+});
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on: http://localhost:3000');
 });
 
 export default app;
