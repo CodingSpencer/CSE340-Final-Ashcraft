942004a53b1a33dd3064e95b4916a6c893511adf
Spencer <sashcraft121@gmail.com>
2026-07-05 01:58:27 -0600

    css working properly again

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+// 1. Import the layouts package
+import expressLayouts from 'express-ejs-layouts'; 
+
 import accountRoutes from './src/routes/account.routes.js';
 import { authContextMiddleware, flashMiddleware, sessionMiddleware } from './src/config/session.js';
 
@@ -12,8 +15,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
+// 2. Tell Express to use the layout middleware
+app.use(expressLayouts); 
+// 3. Set the default layout file (points to src/views/layouts/layout.ejs)
+app.set('layout', 'layouts/layout'); 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('dist'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(sessionMiddleware);
@@ -21,7 +30,8 @@ app.use(flashMiddleware);
 app.use(authContextMiddleware);
 
 app.get('/', (req, res) => {
-  res.render('pages/home/index', { title: 'Home' });
+  // 4. Remove the .ejs extension here (Express adds it automatically based on your view engine)
+  res.render('pages/index', { title: 'Home' });
 });
 
 app.use('/', accountRoutes);
